e8fb928e5ac44c7cd248bc8cb5cb35aa549ae590
sm1l3 <rioly97.vg@gmail.com>
2018-07-02 16:49:53 +0300

    refactor

--- xchange-bx/src/main/java/org/knowm/xchange/bx/service/BxTradeService.java
+++ xchange-bx/src/main/java/org/knowm/xchange/bx/service/BxTradeService.java
@@ -1,73 +1,80 @@
 package org.knowm.xchange.bx.service;
 
-import java.io.IOException;
-import java.util.Collection;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.bx.BxAdapters;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.trade.*;
 import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
 import org.knowm.xchange.service.trade.TradeService;
-import org.knowm.xchange.service.trade.params.CancelOrderByIdParams;
 import org.knowm.xchange.service.trade.params.CancelOrderByPairAndIdParams;
 import org.knowm.xchange.service.trade.params.CancelOrderParams;
 import org.knowm.xchange.service.trade.params.TradeHistoryParams;
 import org.knowm.xchange.service.trade.params.orders.OpenOrdersParams;
 
+import java.io.IOException;
+import java.util.Collection;
+
 public class BxTradeService extends BxTradeServiceRaw implements TradeService {
 
-  public BxTradeService(Exchange exchange) {
-    super(exchange);
-  }
+    public BxTradeService(Exchange exchange) {
+        super(exchange);
+    }
 
-  @Override
-  public OpenOrders getOpenOrders() throws IOException {
-    return getOpenOrders(createOpenOrdersParams());
-  }
+    @Override
+    public OpenOrders getOpenOrders() throws IOException {
+        return getOpenOrders(createOpenOrdersParams());
+    }
 
-  @Override
-  public OpenOrders getOpenOrders(OpenOrdersParams openOrdersParams) throws IOException {
-    return BxAdapters.adaptOpenOrders(getBxOrders());
-  }
+    @Override
+    public OpenOrders getOpenOrders(OpenOrdersParams openOrdersParams) throws IOException {
+        return BxAdapters.adaptOpenOrders(getBxOrders());
+    }
 
-  @Override
-  public String placeMarketOrder(MarketOrder marketOrder) throws IOException {
-    throw new NotAvailableFromExchangeException();
-  }
+    @Override
+    public String placeMarketOrder(MarketOrder marketOrder) throws IOException {
+        throw new NotAvailableFromExchangeException();
+    }
 
-  @Override
-  public String placeLimitOrder(LimitOrder limitOrder) throws IOException {
-    return placeBxLimitOrder(limitOrder);
-  }
+    @Override
+    public String placeLimitOrder(LimitOrder limitOrder) throws IOException {
+        return placeBxLimitOrder(limitOrder);
+    }
 
-  @Override
-  public String placeStopOrder(StopOrder stopOrder) throws IOException {
-    throw new NotAvailableFromExchangeException();
-  }
+    @Override
+    public String placeStopOrder(StopOrder stopOrder) throws IOException {
+        throw new NotAvailableFromExchangeException();
+    }
 
-  @Override
-  public boolean cancelOrder(CancelOrderParams cancelOrderParams) throws IOException {
-    return cancelOrderParams instanceof CancelOrderByPairAndIdParams
-        && cancelBxOrder(((CancelOrderByPairAndIdParams) cancelOrderParams).getOrderId(), ((CancelOrderByPairAndIdParams) cancelOrderParams).getCurrencyPair());
-  }
+    @Override
+    public boolean cancelOrder(String orderId) throws IOException {
+        throw new NotAvailableFromExchangeException();
+    }
 
-  @Override
-  public UserTrades getTradeHistory(TradeHistoryParams tradeHistoryParams) throws IOException {
-    return BxAdapters.adaptUserTrades(getBxTradeHistory(tradeHistoryParams));
-  }
+    @Override
+    public boolean cancelOrder(CancelOrderParams cancelOrderParams) throws IOException {
+        if (!(cancelOrderParams instanceof CancelOrderByPairAndIdParams)) {
+            throw new IllegalArgumentException();
+        }
+        return cancelBxOrder(((CancelOrderByPairAndIdParams) cancelOrderParams).getOrderId(), ((CancelOrderByPairAndIdParams) cancelOrderParams).getCurrencyPair());
+    }
 
-  @Override
-  public TradeHistoryParams createTradeHistoryParams() {
-    return null;
-  }
+    @Override
+    public UserTrades getTradeHistory(TradeHistoryParams tradeHistoryParams) throws IOException {
+        return BxAdapters.adaptUserTrades(getBxTradeHistory(tradeHistoryParams));
+    }
 
-  @Override
-  public OpenOrdersParams createOpenOrdersParams() {
-    return null;
-  }
+    @Override
+    public TradeHistoryParams createTradeHistoryParams() {
+        return null;
+    }
 
-  @Override
-  public Collection<Order> getOrder(String... orderIds) throws IOException {
-    return getBxOrder(orderIds);
-  }
+    @Override
+    public OpenOrdersParams createOpenOrdersParams() {
+        return null;
+    }
+
+    @Override
+    public Collection<Order> getOrder(String... orderIds) throws IOException {
+        return getBxOrder(orderIds);
+    }
 }
