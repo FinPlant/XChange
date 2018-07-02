b9c1d0ba2e6afc61e2f9d9506afd7d0040c26b4f
sm1l3 <rioly97.vg@gmail.com>
2018-07-02 12:54:07 +0300

    cancel bx order fix update

--- xchange-bx/src/main/java/org/knowm/xchange/bx/service/BxTradeServiceRaw.java
+++ xchange-bx/src/main/java/org/knowm/xchange/bx/service/BxTradeServiceRaw.java
@@ -11,6 +11,7 @@ import org.knowm.xchange.bx.dto.trade.results.BxCancelOrderResult;
 import org.knowm.xchange.bx.dto.trade.results.BxCreateOrderResult;
 import org.knowm.xchange.bx.dto.trade.results.BxOrdersResult;
 import org.knowm.xchange.bx.dto.trade.results.BxTradeHistoryResult;
+import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.trade.LimitOrder;
 import org.knowm.xchange.exceptions.ExchangeException;
@@ -18,100 +19,82 @@ import org.knowm.xchange.service.trade.params.TradeHistoryParams;
 
 public class BxTradeServiceRaw extends BxBaseService {
 
-  public BxTradeServiceRaw(Exchange exchange) {
-    super(exchange);
-  }
-
-  public boolean cancelBxOrder(String orderId) throws IOException {
-    String pairId = getBxOrderRaw(orderId)
-            .stream()
-            .findFirst()
-            .orElseThrow(() -> new IOException(String.format("Cant find pair id, for order id [%s]", orderId)))
-            .getPairingId();
-    BxCancelOrderResult result =
-            bx.cancelOrder(
-                    pairId,
-                    orderId,
-                    exchange.getExchangeSpecification().getApiKey(),
-                    exchange.getNonceFactory(),
-                    signatureCreator);
-    checkResult(result);
-    return result.isSuccess();
-  }
-
-  public Map<String, List<BxTradeHistory>> getBxTradeHistory(TradeHistoryParams tradeHistoryParams)
-          throws IOException {
-    String startDate = null;
-    String endDate = null;
-    if (tradeHistoryParams != null) {
-      if (tradeHistoryParams instanceof BxTradeHistoryParams) {
-        startDate = ((BxTradeHistoryParams) tradeHistoryParams).getStartDate();
-        endDate = ((BxTradeHistoryParams) tradeHistoryParams).getEndDate();
-      } else {
-        throw new ExchangeException(
-                "Unsupported class of params: " + tradeHistoryParams.getClass().getName());
-      }
+    public BxTradeServiceRaw(Exchange exchange) {
+        super(exchange);
     }
-    BxTradeHistoryResult result =
-            bx.getTradeHistory(
-                    null,
-                    null,
-                    startDate,
-                    endDate,
-                    exchange.getExchangeSpecification().getApiKey(),
-                    exchange.getNonceFactory(),
-                    signatureCreator);
-    return BxUtils.prepareHistory(checkResult(result));
-  }
 
-  public BxOrder[] getBxOrders() throws IOException {
-    BxOrdersResult result =
-            bx.getOrders(
-                    null,
-                    null,
-                    exchange.getExchangeSpecification().getApiKey(),
-                    exchange.getNonceFactory(),
-                    signatureCreator);
-    return checkResult(result);
-  }
+    public boolean cancelBxOrder(String orderId, CurrencyPair currencyPair) throws IOException {
+        String pairId = BxUtils.createBxCurrencyPair(currencyPair);
+        BxCancelOrderResult result =
+                bx.cancelOrder(
+                        pairId,
+                        orderId,
+                        exchange.getExchangeSpecification().getApiKey(),
+                        exchange.getNonceFactory(),
+                        signatureCreator);
+        checkResult(result);
+        return result.isSuccess();
+    }
 
-  public Collection<Order> getBxOrder(String... orderIds) throws IOException {
-    List<Order> orders = new ArrayList<>();
-    BxOrder[] bxOrders = getBxOrders();
-    for (BxOrder order : bxOrders) {
-      for (String orderId : orderIds) {
-        if (order.getOrderId().equals(orderId)) {
-          orders.add(BxAdapters.adaptOrder(order));
+    public Map<String, List<BxTradeHistory>> getBxTradeHistory(TradeHistoryParams tradeHistoryParams)
+            throws IOException {
+        String startDate = null;
+        String endDate = null;
+        if (tradeHistoryParams != null) {
+            if (tradeHistoryParams instanceof BxTradeHistoryParams) {
+                startDate = ((BxTradeHistoryParams) tradeHistoryParams).getStartDate();
+                endDate = ((BxTradeHistoryParams) tradeHistoryParams).getEndDate();
+            } else {
+                throw new ExchangeException(
+                        "Unsupported class of params: " + tradeHistoryParams.getClass().getName());
+            }
         }
-      }
+        BxTradeHistoryResult result =
+                bx.getTradeHistory(
+                        null,
+                        null,
+                        startDate,
+                        endDate,
+                        exchange.getExchangeSpecification().getApiKey(),
+                        exchange.getNonceFactory(),
+                        signatureCreator);
+        return BxUtils.prepareHistory(checkResult(result));
     }
-    return orders;
-  }
 
-  public Collection<BxOrder> getBxOrderRaw(String... orderIds) throws IOException {
-    List<BxOrder> orders = new ArrayList<>();
-    BxOrder[] bxOrders = getBxOrders();
-    for (BxOrder order : bxOrders) {
-      for (String orderId : orderIds) {
-        if (order.getOrderId().equals(orderId)) {
-          orders.add(order);
+    public BxOrder[] getBxOrders() throws IOException {
+        BxOrdersResult result =
+                bx.getOrders(
+                        null,
+                        null,
+                        exchange.getExchangeSpecification().getApiKey(),
+                        exchange.getNonceFactory(),
+                        signatureCreator);
+        return checkResult(result);
+    }
+
+    public Collection<Order> getBxOrder(String... orderIds) throws IOException {
+        List<Order> orders = new ArrayList<>();
+        BxOrder[] bxOrders = getBxOrders();
+        for (BxOrder order : bxOrders) {
+            for (String orderId : orderIds) {
+                if (order.getOrderId().equals(orderId)) {
+                    orders.add(BxAdapters.adaptOrder(order));
+                }
+            }
         }
-      }
+        return orders;
     }
-    return orders;
-  }
 
-
-  public String placeBxLimitOrder(LimitOrder limitOrder) throws IOException {
-    BxCreateOrderResult result =
-            bx.createOrder(
-                    BxUtils.createBxCurrencyPair(limitOrder.getCurrencyPair()),
-                    BxAdapters.adaptOrderType(limitOrder.getType()),
-                    limitOrder.getOriginalAmount().toString(),
-                    limitOrder.getLimitPrice().toString(),
-                    exchange.getExchangeSpecification().getApiKey(),
-                    exchange.getNonceFactory(),
-                    signatureCreator);
-    return checkResult(result);
-  }
+    public String placeBxLimitOrder(LimitOrder limitOrder) throws IOException {
+        BxCreateOrderResult result =
+                bx.createOrder(
+                        BxUtils.createBxCurrencyPair(limitOrder.getCurrencyPair()),
+                        BxAdapters.adaptOrderType(limitOrder.getType()),
+                        limitOrder.getOriginalAmount().toString(),
+                        limitOrder.getLimitPrice().toString(),
+                        exchange.getExchangeSpecification().getApiKey(),
+                        exchange.getNonceFactory(),
+                        signatureCreator);
+        return checkResult(result);
+    }
 }
